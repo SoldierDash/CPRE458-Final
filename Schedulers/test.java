cb5f342f1a0a3f58dab8263663774e8ca5518d30
Zach Miller <zcm1992@gmail.com>
2014-12-16 00:46:19 -0600

    Updated test cases.

--- Schedulers/test.java
+++ Schedulers/test.java
@@ -3,16 +3,32 @@ package Schedulers;
 public class test {
 
 	public static void main(String args[] ) {
-		PollingScheduler ps = new PollingScheduler(2, 5);
-		ps.addPeriodicTask("PT1", 2, 5);
-		ps.addPeriodicTask("PT2", 4, 15);
-		ps.addAperiodicTask("AT1", 3, 1);
-		ps.addAperiodicTask("AT2", 6, 2);
-		ps.addAperiodicTask("AT3", 11, 1);
-		ps.addAperiodicTask("AT4", 11, 1);
+		/* Test 1 */
+		/*
+		DeferrableScheduler ps1 = new DeferrableScheduler(2, 5);
+		ps1.addPeriodicTask("PT1", 2, 5);
+		p1s.addPeriodicTask("PT2", 4, 15);
+		ps1.addAperiodicTask("AT1", 3, 1);
+		ps1.addAperiodicTask("AT2", 6, 2);
+		ps1.addAperiodicTask("AT3", 11, 1);
+		ps1.addAperiodicTask("AT4", 11, 1);
 		
 		for (int i = 0; i < 15; i++) {
-			System.out.println(ps.getNextTask());
+			System.out.println(ps1.getNextTask());
+		}
+		*/
+		
+		/* Test 2 */
+		DeferrableScheduler ps2 = new DeferrableScheduler(2, 5);
+		ps2.addPeriodicTask("PT1", 1, 4);
+		ps2.addPeriodicTask("PT2", 2, 6);
+		ps2.addAperiodicTask("AT1", 2, 2);
+		ps2.addAperiodicTask("AT2", 8, 1);
+		ps2.addAperiodicTask("AT3", 12, 2);
+		ps2.addAperiodicTask("AT4", 19, 1);
+		
+		for (int i = 0; i < 24; i++) {
+			System.out.println(ps2.getNextTask());
 		}
 	}
 	
