cf74696fec79f59aa0839316ed1df15b17cd0810
Zach Miller <zcm1992@gmail.com>
2014-12-19 13:51:01 -0600

    Removed unnecessary print statement.

--- Schedulers/Scheduler.java
+++ Schedulers/Scheduler.java
@@ -155,7 +155,6 @@ public abstract class Scheduler {
 		
 		double exponent = (1.0 / (periodicTasks.size()));
 		double max = (periodicTasks.size()) * (Math.pow(2, exponent) - 1);
-		System.out.println(sum + " <= " + max);
 		return sum <= max;
 	}
 	
